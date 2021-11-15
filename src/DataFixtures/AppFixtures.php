c33179a0f3949a0fdc09cb43f27ad782323fc5de
petrero <petrero@gmail.com>
2021-11-15 10:20:08 +0200

    3.1. Saving Relations
      Our answer table has a new question_id column. Cool... but how do we populate that column? How do we relate an Answer to a Question? This is actually pretty easy... but it might feel weird if you're used to working with databases directly.
    
      Open up src/DataFixtures/AppFixtures.php. We're using Foundry to add rich fixtures, or fake data, into our project.
    
      But to see how relationships work, let's do some good ol' fashioned manual coding.
    
      Creating some Dummy Question and Answer Objects
      - Start by creating a new Answer object... and populate it with enough data to get it to save. Repeat this to create a new Question object... and also give that some data.
    
      Save these boring objects to the database by calling $manager->persist() on both of them.
    
      Cool. If we stop now, these objects won't be related... and the Answer won't even save! Try it:
    
        symfony console doctrine:fixtures:load
      And... woh! My bad! We generated a migration in the last chapter, and then I totally forgot to run it! Time to do that:
    
        symfony console doctrine:migrations:migrate
      Now try the fixtures:
    
        symfony console doctrine:fixtures:load
    
      JoinColumn
      - That's the error I expected:
    
        question_id cannot be null on the answer table
    
      That's because we made question_id required: it was one of the questions that make:entity command asked us. Oh, and I can show you where this is configured. Open up the Answer class and find the question property. It's this JoinColumn(nullable=false): that makes the question_id column required.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Answer;
 use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -15,6 +16,17 @@ class AppFixtures extends Fixture {
 			->unpublished()
 			->many(5)
 			->create();
+
+		$answer = new Answer();
+		$answer->setContent('This question is the best? I wish... I knew the answer.');
+		$answer->setUsername('weaverryan');
+
+		$question = new Question();
+		$question->setName('How to un-disappear your wallet.');
+		$question->setQuestion('... I should not have done this...');
+
+		$manager->persist($answer);
+		$manager->persist($question);
 
 		$manager->flush();
 	}
