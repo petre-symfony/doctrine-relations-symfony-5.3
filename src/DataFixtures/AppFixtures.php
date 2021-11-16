cdc03f040b2f09fa2ccd45c0b8931b7e59627e5b
petrero <petrero@gmail.com>
2021-11-16 11:22:52 +0200

    8.2. Owning Vs Inverse Sides of a Relation
      Cool! Let's relate them! But this time, instead of saying, $answer1->setQuestion(), do it with $question->addAnswer($answer1)... and $question->addAnswer($answer2).
    
      If you think about it... this is really saying the same thing as when we set the relationship from the other direction: this Question has these two answers.
    
      Let's see if it saves! Run the fixtures:
    
        symfony console doctrine:fixtures:load
      And... no errors! I think it worked! Double-check with:
    
        SELECT * FROM answer
    
        symfony console doctrine:query:sql 'SELECT * FROM answer'
      Let's see... yea! Here are the new answers. Oh, apparently I called them both "answer 1" - silly Ryan. But more importantly, each answer is correctly related to a Question.
    
      Ok! so it turns out you can set data from both sides. The two sides of the relationship apparently behave identically.
    
      Now, at this point, you might be saying to yourself:
    
        Why is this guy taking so much time to show me that something works exactly like I expect it too?

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -29,11 +29,15 @@ class AppFixtures extends Fixture {
 		$answer1->setContent('answer 1');
 		$answer1->setUsername('weaverryan');
 		$answer2 = new Answer();
-		$answer2->setContent('answer 1');
+		$answer2->setContent('answer 2');
 		$answer2->setUsername('weaverryan');
+
+		$question->addAnswer($answer1);
+		$question->addAnswer($answer2);
+
 		$manager->persist($answer1);
 		$manager->persist($answer2);
-		
+
 		$manager->flush();
 	}
 }
